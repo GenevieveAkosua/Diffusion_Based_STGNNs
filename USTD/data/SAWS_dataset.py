be8566ef9c7c2ecd4efcc782cdf40ab4bb573938
Génevieve Chikwanha <chkkar002@myuct.ac.za>
2026-08-12 20:57:57 +0200

    Adding an SAWS dataset class for USTD

--- USTD/data/SAWS_dataset.py
+++ USTD/data/SAWS_dataset.py
@@ -1,4 +1,5 @@
-# implemented by p0werHu
+# Author: Genevieve Chikwanha
+# Date: 10 August 2026
 from data.base_dataset import BaseDataset
 import torch
 import pandas as pd
@@ -14,93 +15,51 @@ class SAWSDataset(BaseDataset):
     """
     Note that the beijing air quality dataset contains a lot of missing values, we need to handle this explicitly.
     """
+
     @staticmethod
     def modify_commandline_options(parser, is_train):
-        """Add new dataset-specific options, and rewrite default values for existing options.
-        Parameters:
-            parser          -- original option parser
-            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
-        Returns:
-            the modified parser.
-        """
-        parser.set_defaults(y_dim=1, covariate_dim=0)
+        parser.set_defaults(y_dim=1, covariate_dim=0, spatial_dim=64)
         return parser
 
-    def __init__(self, opt, aq_location_path, data_path, test_nodes_path):
-        super().__init__(opt)
-        """
-        load data give options
-        """
+    def __init__(self, opt):
+        BaseDataset.__init__(self, opt)
+        self.target_channel = 0
         self.opt = opt
-
-        self.pred_attrs = opt.pred_attr
-        assert self.pred_attrs == 'PM25', 'For other attributes, signals of several nodes are completely missing. Need to use other preprocessing methods.'
-        self.time_division = {
-            'train': [0.0, 0.6],
-            'val': [0.6, 0.8],
-            'test': [0.8, 1.0]
-        }
-
-        #self.A = self.load_loc(aq_location_path)
-		self.A = self.load_loc(d"dataset/saws/adj.npy")
-        #self.raw_data = self.load_feature(data_path, self.time_division[opt.phase])
-		self.raw_data = self.load_feature("dataset/saws/flow.npy", self.time_division[opt.phase])
-
+        self.time_division = {'train': [0.0, 0.6], 'val': [0.6, 0.8],'test': [0.8, 1.0]}
+        self.A = np.load("dataset/saws/adj.npy")
+        self.raw_data = self.load_feature("dataset/saws/flow.npy", self.time_division[opt.phase])
         # get data division index
         self.opt.__dict__.update({'num_nodes': self.A.shape[0]})
-        self.test_node_index = self.get_node_division(test_nodes_path, num_nodes=self.raw_data['pred'].shape[0])
-        self.train_node_index = np.setdiff1d(np.arange(self.raw_data['pred'].shape[0]), self.test_node_index)
+        self.test_node_index = np.array([], dtype=np.int64)          # no nodes held out spatially
+        self.train_node_index = np.arange(self.raw_data['pred'].shape[0])  # all stations used
 
         # data format check
         self._data_format_check()
 
-    def load_loc(self, aq_location_path):
-        """
-        Args:
-        Returns:
-
-        """
-        print('Loading station locations...')
-        # load weather station locations data
-        adj = np.load(aq_location_path)
-        return adj
-
     def load_feature(self, data_path, time_division):
-        raw_data = pd.read_hdf(data_path, key='data')
-        covariates = pd.read_hdf(data_path, key='covariate')
-        missing = pd.read_hdf(data_path, key='missing')
+        flow = np.load(data_path).astype(np.float32) # T, V, D
+        X = np.transpose(flow, (1, 0, 2)).copy() # V, T, D
+        num_nodes, num_time, num_channels = X.shape
+		X = X[:, :, self.target_channel:self.target_channel + 1] # V, T, 1
+        self.opt.__dict__.update({'y_dim': 1})
 
-        print('Loading weather features...')
-        data = {'feat': [],
-                'pred': [],
-                'missing': [],
-                'time': []}
+        train_start = int(self.time_division['train'][0] * num_time)
+        train_end = int(self.time_division['train'][1] * num_time)
+        X_train_slice = X[:, train_start:train_end, :]
+        channel_mean = np.mean(X_train_slice, axis=(0, 1)).astype(np.float32)
+        channel_std = np.std(X_train_slice, axis=(0, 1)).astype(np.float32)
+        channel_std[channel_std == 0] = 1.0
+		self.add_norm_info(channel_mean, channel_std)
+        X = (X - self.opt.mean) / self.opt.scale
+        missing = np.zeros_like(X, dtype=np.float32)
+        start_time = np.datetime64('2016-01-01T01:00:00')
+        full_timestamps = start_time + np.arange(num_time, dtype='int64') * np.timedelta64(1, 'h')
+        full_time_seconds = ((full_timestamps - np.datetime64('1970-01-01T00:00:00')) / np.timedelta64(1, 's')).astype(np.int64)
 
-        for id, station_aq in raw_data.groupby('station_id'):
-            station_aq = station_aq.set_index("time").drop(columns=['station_id'])
-            # split data into features and labels
-            data['pred'].append(station_aq[self.pred_attrs].to_numpy()[np.newaxis])
-            data['missing'].append(missing.loc[missing['station_id'] == id][self.pred_attrs + '_Missing'].to_numpy()[np.newaxis])
-            data['feat'].append(covariates.loc[covariates['station_id'] == id].drop(columns=['station_id', 'time']).to_numpy()[np.newaxis])
+        start_index = int(time_division[0] * num_time)
+        end_index = int(time_division[1] * num_time)
+        X = X[:, start_index:end_index, :]
+        missing = missing[:, start_index:end_index, :]
+        time_list = full_time_seconds[start_index:end_index]
 
-        data['pred'] = np.concatenate(data['pred'], axis=0)[..., np.newaxis]
-        data['missing'] = np.concatenate(data['missing'], axis=0)[..., np.newaxis]
-        data['feat'] = np.concatenate(data['feat'], axis=0)
-
-        # normalize data
-        self.add_norm_info(np.mean(data['pred']), np.std(data['pred']))
-        data['pred'] = (data['pred'] - self.opt.mean) / self.opt.scale
-
-        data_length = data['feat'].shape[1]
-        start_index, end_index = int(time_division[0] * data_length), int(time_division[1] * data_length)
-        data['feat'] = data['feat'][:, start_index:end_index, :]
-        data['missing'] = data['missing'][:, start_index:end_index, :]
-        data['pred'] = data['pred'][:, start_index:end_index, :]
-
-        data['time'] = station_aq[start_index:end_index].index.values.astype(np.datetime64)
-        data['time'] = ((data['time'] - np.datetime64('1970-01-01T00:00:00')) / np.timedelta64(1, 's'))
-        # todo: I don't use covariate for diffusion models anymore
-        data.pop('feat')
-        return data
-
-
+        return {'pred': X.astype(np.float32), 'missing': missing.astype(np.float32), 'time': time_list}
